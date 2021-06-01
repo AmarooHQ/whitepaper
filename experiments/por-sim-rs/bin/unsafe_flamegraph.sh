b62aab5d5435e031bf3719109caf1ccd570df2c2
Max Kaye <m@xk.io>
2021-06-01 19:54:46 +1000

    add env export in script

--- experiments/por-sim-rs/bin/unsafe_flamegraph.sh
+++ experiments/por-sim-rs/bin/unsafe_flamegraph.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export RUST_LOG=warn
+
 echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid
 cargo flamegraph --bin por-sim-rs -- -S WeightedDag -n 100 -r 0.3 -s 6000 && explorer.exe flamegraph.svg
 echo 2 | sudo tee /proc/sys/kernel/perf_event_paranoid
