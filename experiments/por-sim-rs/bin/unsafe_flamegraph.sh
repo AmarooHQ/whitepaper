1c9a7940ef91540cf41e29f307d74d5e70d9bb31
Max Kaye <m@xk.io>
2021-06-08 12:35:39 +1000

    Update flamegraph bin

--- experiments/por-sim-rs/bin/unsafe_flamegraph.sh
+++ experiments/por-sim-rs/bin/unsafe_flamegraph.sh
@@ -3,5 +3,7 @@
 export RUST_LOG=warn
 
 echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid
-cargo flamegraph --bin por-sim-rs -- -S WeightedDag -n 100 -r 0.3 -s 20000 && explorer.exe flamegraph.svg
+# cargo flamegraph --bin por-sim-rs -- -S WeightedDag -n 100 -r 0.3 -s 20000 \
+cargo flamegraph -- -s 20000 -e 60000 -n 100 -r 0.4 -R SelfishMining -S SimpleChain \
+  && explorer.exe flamegraph.svg
 echo 2 | sudo tee /proc/sys/kernel/perf_event_paranoid
