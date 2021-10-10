8d23d03dcc09d677c092de9887c4e0d2fb99168b
Max Kaye <m@xk.io>
2021-10-10 21:32:47 +1100

    few changes

---
+++ includes/ut/complexity/ut-complexity-gen-purs/src/Amaroo/WP/Conversion.purs
@@ -0,0 +1,22 @@
+module Amaroo.WP.Conversion where
+
+import Prel
+
+{-|
+
+Quickcheck conversion properties.
+
+In ./includes/ut/20-por/30-comparing-work-3.tex I mention some properties that need to hold.
+Sweet, property based testing time.
+
+Additionally, it means defining the conversion functions more specifically.
+
+Example checks:
+- does measuring chain-weight in coins work like predicted?
+
+|-}
+
+weightOf_coinsSimple :: Block -> State -> Number
+weightOf_coinsSimple b state = blockRewardOfAt_coinsSimple (chainOf b) (b.timestamp) state
+
+weightOf_
