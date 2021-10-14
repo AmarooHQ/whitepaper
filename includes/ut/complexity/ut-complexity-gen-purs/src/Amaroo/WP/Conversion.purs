152e7a5fbd715fbf10b95750e17612876c15958d
Max Kaye <m@xk.io>
2021-10-15 10:32:38 +1100

    more type-level stuff

--- includes/ut/complexity/ut-complexity-gen-purs/src/Amaroo/WP/Conversion.purs
+++ includes/ut/complexity/ut-complexity-gen-purs/src/Amaroo/WP/Conversion.purs
@@ -2,6 +2,8 @@ module Amaroo.WP.Conversion where
 
 import Prel
 
+import Prelude (class Semiring)
+
 -- import Data.Tuple (Tuple(..))
 -- import Prelude (class Semiring)
 
@@ -75,8 +77,13 @@ data Seconds
 newtype Frac :: forall k j. k -> j -> Type
 newtype Frac n d = Frac Number
 
+unfrac :: forall a b. Frac a b -> Number
+unfrac (Frac n) = n
+
 type Units' :: forall k. k -> Type
 type Units' s = Frac s Unit
+
+type FracU n d = Frac (Units' n) (Units' d)
 
 derive newtype instance showHashes :: Show (Hashes a)
 derive newtype instance showCoins :: Show (Coins a)
@@ -88,69 +95,64 @@ derive newtype instance showFrac :: Show (Frac a b)
 -- instance convConvWork :: Conv _ (Hashes "R") (Hashes "L") where
 --   conv = convWorkSafe
 
--- type Params =
---   { lf :: Frac (Blocks "L") (Seconds)
---   , lr :: Frac (Coins "L") (Blocks "L")
---   , ld :: Frac (Hashes "L") (Blocks "L")
---   , rf :: Frac (Blocks "R") (Seconds)
---   , rr :: Frac (Coins "R") (Blocks "R")
---   , rd :: Frac (Hashes "R") (Blocks "R")
---   , xrl :: Frac (Coins "L") (Coins "R")
---   }
+type Xr2l = FracU (Coins "L") (Coins "R")
+
+type Rd = (FracU (Hashes "R") (Blocks "R"))
+type Rdp = (FracU (Hashes "R") (Seconds))
+type Rf = (FracU (Blocks "R") (Seconds))
+type Rr = (FracU (Coins "R") (Blocks "R"))
+type Rhpc = (FracU (Hashes "R") (Coins "R"))
+
+type Ld = (FracU (Hashes "L") (Blocks "L"))
+type Ldp = (FracU (Hashes "L") (Seconds))
+type Lf = (FracU (Blocks "L") (Seconds))
+type Lr = (FracU (Coins "L") (Blocks "L"))
+type Lhpc = (FracU (Hashes "L") (Coins "L"))
+
 
 type Params =
-  { lf :: Number
-  , lr :: Number
-  , ld :: Number
-  , rf :: Number
-  , rr :: Number
-  , rd :: Number
-  , xrl :: Number
+  { lf :: Lf
+  , lr :: Lr
+  , ld :: Ld
+  , rf :: Rf
+  , rr :: Rr
+  , rd :: Rd
+  , xrl :: Xr2l
   }
 
--- zeroParams :: Params
--- zeroParams =
---   { lf: Frac 0.0
---   , lr: Frac 0.0
---   , ld: Frac 0.0
---   , rf: Frac 0.0
---   , rr: Frac 0.0
---   , rd: Frac 0.0
---   , xrl: Frac 0.0
---   }
-
--- oneParams :: Params
--- oneParams =
---   { lf: Frac 1.0
---   , lr: Frac 1.0
---   , ld: Frac 1.0
---   , rf: Frac 1.0
---   , rr: Frac 1.0
---   , rd: Frac 1.0
---   , xrl: Frac 1.0
---   }
+invertParams :: Params -> Params
+invertParams p =
+  { lf: Frac (unfrac p.rf)
+  , lr: Frac (unfrac p.rr)
+  , ld: Frac (unfrac p.rd)
+  , rf: Frac (unfrac p.lf)
+  , rr: Frac (unfrac p.lr)
+  , rd: Frac (unfrac p.ld)
+  , xrl: Frac (1.0 / unfrac p.xrl)
+  }
 
 zeroParams :: Params
 zeroParams =
-  { lf: 0.0
-  , lr: 0.0
-  , ld: 0.0
-  , rf: 0.0
-  , rr: 0.0
-  , rd: 0.0
-  , xrl: 0.0
+  { lf: Frac 0.0
+  , lr: Frac 0.0
+  , ld: Frac 0.0
+  , rf: Frac 0.0
+  , rr: Frac 0.0
+  , rd: Frac 0.0
+  , xrl: Frac 0.0
   }
 
 oneParams :: Params
 oneParams =
-  { lf: 1.0
-  , lr: 1.0
-  , ld: 1.0
-  , rf: 1.0
-  , rr: 1.0
-  , rd: 1.0
-  , xrl: 1.0
+  { lf: Frac 1.0
+  , lr: Frac 1.0
+  , ld: Frac 1.0
+  , rf: Frac 1.0
+  , rr: Frac 1.0
+  , rd: Frac 1.0
+  , xrl: Frac 1.0
   }
+
 
 class Units a where
   getRaw :: a -> Number
@@ -177,14 +179,14 @@ instance genInvert :: (Units a, Units x, Units b, GetCC a x b) => Invertable a x
   vnoc p b x = makeFrom $ getRaw b / getConv (convAB p :: ConvConst a x b) / getRaw x
 
 
-instance crPbrTclPbl :: (Units x) => HasConst (Frac (Coins "R") (Blocks "R")) x (Frac (Coins "L") (Blocks "L")) where
-  convAB {lf, xrl, rf} = ConvConst (rf / lf * xrl)
+-- instance crPbrTclPbl :: (Units x) => HasConst (Frac (Coins "R") (Blocks "R")) x (Frac (Coins "L") (Blocks "L")) where
+--   convAB {lf, xrl, rf} = ConvConst (rf / lf * xrl)
 
-asdf :: Frac (Coins "L") (Blocks "L")
-asdf = conv (zeroParams {lf = 2.0, xrl = 2.0, rf = 1.0}) (Frac 0.88 :: Frac (Coins "R") (Blocks "R")) (Coins 1.0 :: Coins "R")
+-- asdf :: Frac (Coins "L") (Blocks "L")
+-- asdf = conv (zeroParams {lf = 2.0, xrl = 2.0, rf = 1.0}) (Frac 0.88 :: Frac (Coins "R") (Blocks "R")) (Coins 1.0 :: Coins "R")
 
-convReward2 :: Params -> Frac (Coins "R") (Blocks "R") -> Frac (Coins "L") (Blocks "L")
-convReward2 p rr = conv p rr (Coins 1.0)
+-- convReward2 :: Params -> Frac (Coins "R") (Blocks "R") -> Frac (Coins "L") (Blocks "L")
+-- convReward2 p rr = conv p rr (Coins 1.0)
 
 -- class ConvToRate a b o | a b -> o
 
@@ -203,17 +205,12 @@ instance uFrac :: Units (Frac a b) where
 newtype FromTo :: forall k1 k2. k1 -> k2 -> Type
 newtype FromTo a b = FromTo Number
 
-type Rd = (Frac (Hashes "R") (Blocks "R"))
-type Rdp = (Frac (Hashes "R") (Seconds))
-type Rf = (Frac (Blocks "R") (Seconds))
-type Rr = (Frac (Coins "R") (Blocks "R"))
-type Rhpc = (Frac (Hashes "R") (Coins "R"))
 
-convD2Dp :: Params -> FromTo Rd Rdp
-convD2Dp {rf} = FromTo rf
+-- convD2Dp :: Params -> FromTo Rd Rdp
+-- convD2Dp {rf} = FromTo rf
 
-convR2D :: Params -> FromTo Rr Rhpc
-convR2D {rd, rr} = FromTo (rd / rr / rr)
+-- convR2D :: Params -> FromTo Rr Rhpc
+-- convR2D {rd, rr} = FromTo (rd / rr / rr)
 
 
 
@@ -282,22 +279,100 @@ multDN :: forall a b c. Frac a b -> Frac b c -> Frac a c
 multDN (Frac x) (Frac y) = Frac (x * y)
 
 multND :: forall a b c. Frac a b -> Frac c a -> Frac c b
-multND (Frac x) (Frac y) = Frac (x * y)
+multND = flip multDN
+-- multND (Frac x) (Frac y) = Frac (x * y)
+
+-- instance semiringFrac :: Semiring (Frac a b) where
+--   add :: (Frac a b) -> (Frac a b) -> Frac a b
+--   add (Frac x) (Frac y) = Frac (x + y)
+--   zero = Frac zero
+--   one = Frac one
+--   -- this isn't right w/ units :/
+--   mul :: (Frac a b) -> (Frac a b) -> Frac a b
+--   mul (Frac x) (Frac y) = Frac (x * y)
 
 divDD :: forall a b c. Frac a b -> Frac c b -> Frac a c
 divDD x y = multDN x (invert y)
 
-divNN :: forall a b c. Frac a b -> Frac a c -> Frac c b
+divNN :: forall d e n. Frac n d -> Frac n e -> Frac e d
 divNN x y = multND x (invert y)
 
-multD :: forall a b. Frac a (Units' b) -> (Units' b) -> Units' a
+div' :: forall a d b e. Frac a d -> Frac b e -> Frac (Frac a d) (Frac b e)
+div' (Frac x) (Frac y) = Frac (x * y)
+
+mul' :: forall a d b e. Frac a d -> Frac b e -> Frac (Frac a d) (Frac e b)
+mul' (Frac x) (Frac y) = Frac (x * y)
+
+multD :: forall a b. (Units' b) -> Frac (Units' a) (Units' b) -> Units' a
 multD (Frac x) (Frac y) = Frac (x * y)
 
-divN :: forall a b. Frac (Units' a) b -> (Units' a) -> Frac Unit b
-divN (Frac x) (Frac y) = Frac (x / y)
+divN :: forall a b. (Units' a) -> Frac (Units' a) b -> Frac Unit b
+divN (Frac y) (Frac x) = Frac (y / x)
 
 div_ :: forall a b. Frac a b -> Frac a b -> Frac Unit Unit
 div_ (Frac x) (Frac y) = Frac (x / y)
 
 mul_ :: forall a b. Frac a b -> Frac b a -> Frac Unit Unit
 mul_ (Frac x) (Frac y) = Frac (x * y)
+
+mulRate :: forall a b m n. Frac a b -> Frac (Frac m n) (Frac a b) -> Frac m n
+mulRate (Frac x) (Frac y) = Frac (x * y)
+
+cancelD :: forall a b c. Frac (Frac a b) (Frac c b) -> Frac a c
+cancelD (Frac x) = Frac x
+
+cancelN :: forall a b c. Frac (Frac a b) (Frac a c) -> Frac c b
+cancelN (Frac x) = Frac x
+
+--| reorg, swap denom of numerator with numerator of denom.
+--| (x/y) / (a/b) = bx/ay = (x/a) / (y/b)
+swapDN :: forall a b c d. Frac (Frac a b) (Frac c d) -> Frac (Frac a c) (Frac b d)
+swapDN (Frac x) = Frac x
+
+--| reorg, swap numerator of numerator with denom of denom.
+--| (x/y) / (a/b) = bx/ay = (b/y) / (a/x)
+swapND :: forall a b c d. Frac (Frac a b) (Frac c d) -> Frac (Frac d b) (Frac c a)
+swapND (Frac x) = Frac x
+
+test1 :: Params -> Units' (Blocks "R") -> (Units' (Hashes "R"))
+test1 {rd} b = multD b rd
+
+-- test11 :: Params -> FracU (Coins "R")
+
+test2 :: Params -> Rd -> Ld
+test2 {ld, lr, xrl, rr, rf, lf} rd = lhpb
+  where
+    rcph = (divDD rr rd) :: FracU (Coins "R") (Hashes "R")
+    -- rcplb = ?asdf :: FracU (Coins "R") (Blocks "L")
+    lcprh = (multDN xrl rcph) :: FracU (Coins "L") (Hashes "R")
+    lbprh = divNN lcprh lr :: FracU (Blocks "L") (Hashes "R")
+    lhprh = multND lbprh ld :: FracU (Hashes "L") (Hashes "R")
+    lhprb = multDN lhprh rd :: FracU (Hashes "L") (Blocks "R")
+    lhps = multDN lhprb rf :: FracU (Hashes "L") (Seconds)
+    lhpb = divDD lhps lf :: FracU (Hashes "L") (Blocks "L")
+
+test3 :: Params -> Rr -> Ld
+test3 {ld, lr, xrl} rr = res
+  where
+    ldplr = divDD ld lr :: FracU (Hashes "L") (Coins "L")
+    lcprb = multDN xrl rr :: FracU (Coins "L") (Blocks "R")
+    lhprb = multND lcprb ldplr :: FracU (Hashes "L") (Blocks "R")
+    -- rhplh = divDD rd lhprb :: FracU (Hashes "R") (Hashes "L")
+    -- lc2lh = divDD ld lr :: FracU (Hashes "L") (Coins "L")
+    -- rc2rh = divDD rd rr :: FracU (Hashes "R") (Coins "R")
+    rb2lb = divNN (multND rr xrl) lr :: FracU (Blocks "L") (Blocks "R")
+    res = divDD lhprb rb2lb
+
+    -- lhprh = multDN ldplr lcprb :: FracU (Hashes "L") (Hashes "R")
+    -- lhprh = multDN ldplr lcprd :: FracU (Hashes "L") (Hashes "R")
+
+test4 :: Params -> _ -> Frac _ _
+test4 {rd, rf, rr, ld, lf, lr, xrl} x = rfplf `mul'` ldplr `mul'` (multND rr xrl)
+  where
+    rrp = multDN rr rf :: FracU (Coins "R") (Seconds)
+    rrplb = divDD rrp lf :: FracU (Coins "R") (Blocks "L")
+    ldplr = cancelD $ div' ld lr
+    rfplf = divDD rf lf :: FracU (Blocks "R") (Blocks "L")
+
+convWork3 :: Params -> Units' (Hashes "R") -> Frac (Hashes "L") Unit
+convWork3 {ld, lr, xrl, rd, rr} x = multD x $ multDN (divDD ld lr) (multDN xrl (divDD rr rd))
