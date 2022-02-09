8839a8591719d8dae91605f19dd5fa56f5194ed8
Max Kaye <m@xk.io>
2022-02-09 21:10:44 +1100

    add prediction to run_n_times script

--- experiments/por-sim-rs/bin/run_n_times.sh
+++ experiments/por-sim-rs/bin/run_n_times.sh
@@ -43,3 +43,7 @@ echo "Trials: ${N_TRIALS} -- Successes: ${success}; Failures: ${failure} (${SIMU
 # N_CHAINS=3
 # - Trials: 100 -- Successes: 29; Failures: 71
 # - runtime: 45 s (last: 460 ms)
+
+# prediction: as N_1 (the number of simplex chains) grows, X confirmations on one chain is as secure as N_1 * X confirmations. like, for a doublespend to succeed, if you wait X confirmations on a chain, then the doublespend (with given parameters) occurs as often as if it were waiting for N_1 * X confirmations on a traditional chain (e.g. bitcoin, ethereum).
+# - this is based on Meni Rosenfeld's 2012 paper on confirmation dynamics Analysis of hashrate-based double-spending (archive.org). basically: security depends on # of confirmations, not time. so if N_1 chains, the confirmation rate is N_1*B_f, so we get N_1 times as many confirmations as w/ a single chain. The prediction is basically just a restatement of that.
+# - note: there might be issues w/ the simulator at some point b/c of the architecture (everything is done in 'steps' w/ a block target time of 10 steps, which means 10% of blocks would be 'stale' in bitcoin parlance)
